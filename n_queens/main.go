09b603ec2b0ff4daad219be29c1805707390c7bc
harry <duongminhnam99@gmail.com>
2022-04-22 16:40:53 +0700

    [Update] n queens

--- n_queens/main.go
+++ n_queens/main.go
@@ -28,13 +28,32 @@ func printBoard() {
 }
 
 // check if the queen won't be check
-func isSafe() bool {
-	return false
+func isSafe(row int, col int) bool {
+	for i := 0; i <= N; i++ {
+		if board[row][i] == 1 {
+			return false
+		}
+	}
+
+	for r, c := row, col; r >= 0 && c >= 0; r, c = r-1, c-1 {
+		if board[r][c] == 1 {
+			return false
+		}
+	}
+
+	for a, b := row, col; b >= 0 && a < N; a, b = a+1, b-1 {
+		if board[a][b] == 1 {
+			return false
+		}
+	}
+
+	return true
+
 }
 
 func solve(col int) bool {
 	for i := 0; i < N; i++ {
-		if isSafe() {
+		if isSafe(i, col) {
 			board[i][col] = 1
 			if solve(col + 1) {
 				return true
